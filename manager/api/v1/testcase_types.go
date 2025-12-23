b3913df4d458a3bb0c44dbac0250d5aa3af7dff8
mattisonchao <mattisonchao@gmail.com>
2025-12-23 19:05:01 +0800

    revert some changes

--- manager/api/v1/testcase_types.go
+++ manager/api/v1/testcase_types.go
@@ -60,6 +60,7 @@ func (tc *TestCase) OpRate() int {
 }
 
 // +kubebuilder:object:root=true
+
 type TestCaseList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
